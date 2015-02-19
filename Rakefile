f4d9a16d017548a1e0a939b0f544d67a1ef10c5b
Joy Paas <kjcpaas@gmail.com>
2015-02-20 00:13:24 +0800

    Add Rakefile to run everything

---
+++ Rakefile
@@ -0,0 +1,7 @@
+task :default => [:run_all]
+
+task :run_all do
+  Dir["*.rb"].each do |file|
+    ruby file
+  end
+end
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -7,12 +7,22 @@ def benchmark_solution(expected_answer)
   end
 
   p "Time taken: #{time_taken} seconds"
+  p time_taken < 60 ? "Benchmark PASSED!" : "Benchmark FAILED!"
+  p ""
 end
 
 def check_answer(answer, expected_answer)
-  p "Your answer is #{answer}"
+  strings = [ "My Answer: #{answer}" ]
   if expected_answer
-    p "Answer should be: #{expected_answer}"
-    p "#{ expected_answer == answer ? 'Correct' : 'Wrong' }!"
+    strings << "Correct Answer: #{expected_answer}"
+    strings << "#{ expected_answer == answer ? 'Correct' : 'Wrong' }!"
   end
+  p strings.join(" | ")
+end
+
+def show_problem_info(info)
+  p "======================================================"
+  p "  Problem ##{info[:number]}"
+  p "  #{info[:title]}"
+  p "======================================================"
 end
